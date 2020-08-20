52ce09c3ec1568474b5ddfa482b8f0bd3836f831
Daniel Bernhardt <54528594+danielbernhardt1996@users.noreply.github.com>
2020-08-20 14:41:59 +0200

    Added method getName

--- src/com/company/Fortune.java
+++ src/com/company/Fortune.java
@@ -1,4 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Fortune {
+    Scanner sc = new Scanner(System.in);
+    String[] fortunes = {};
+
+    int name, color, food;
+
+    void getName(){
+        System.out.println("What is your name?");
+        String scName = sc.next();
+        name = scName.length();
+    }
+
+
 }
