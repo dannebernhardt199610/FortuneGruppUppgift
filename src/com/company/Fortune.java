592330f503e89bd94986d713f5114fa8735a0796
Daniel Bernhardt <54528594+danielbernhardt1996@users.noreply.github.com>
2020-08-20 14:53:58 +0200

    Filled with fortune messages

--- src/com/company/Fortune.java
+++ src/com/company/Fortune.java
@@ -4,7 +4,17 @@ import java.util.Scanner;
 
 public class Fortune {
     Scanner sc = new Scanner(System.in);
-    String[] fortunes = {};
+    String[] fortunes = {  "A dubious friend may be an enemy in camouflage.",
+            "Your success will astonish everyone.",
+            "You will soon be surrounded by good friends and laughter.",
+            "Many will travel to hear you speak.",
+            "Now is a good time to buy stock.",
+            "Physical activity will dramatically improve your outlook today.",
+            "Say hello to others. You will have a happier day.",
+            "You should be able to undertake and complete anything.",
+            "You will be pleasantly surprised tonight.",
+            "You will be traveling and coming into a fortune.",
+    };
 
     int name, color, food;
 
